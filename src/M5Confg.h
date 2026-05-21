6f4314d784a787de1d86dcb4f006384b5a767060
SGM24 <beckers.rival@gmail.com>
2026-05-20 22:28:24 -0400

    renamed class and modified .h for the lib

--- src/M5Confg.h
+++ src/M5Confg.h
@@ -1,15 +1,15 @@
-#ifndef M5SDE_H
-#define M5SDE_H
+#ifndef M5CONFIG_H
+#define M5CONFIG_H
+#ifndef MAX_DEPTH
+    #define MAX_DEPTH 8
+#endif
 #include <stdint.h>
-#include <vector>
 #include <Arduino.h>
 #include <M5GFX.h>
 extern M5Canvas canvas;
-class M5SDE{
+class M5Config{
     public:
-    typedef void (*SelectionCallback)(const char* path); // returns the absolute path of selected option
     struct ExplorerTheme{
-        uint16_t directory_color = YELLOW;
         uint16_t background_color = BLACK;
         uint16_t border_color = WHITE;
         uint16_t selection_color = BLUE;
@@ -19,40 +19,65 @@ class M5SDE{
         const lgfx::v1::IFont* font = &fonts::Font2;
     };
 
+    enum ConfigType{
+        TYPE_UINT8_T,
+        TYPE_UINT16_T,
+        TYPE_UINT32_T,
+        TYPE_INT8_T,
+        TYPE_INT16_T,
+        TYPE_INT32_T,
+        TYPE_BOOL,
+        TYPE_ENUM,
+        TYPE_STRING,
+        TYPE_SUBMENU,
+        TYPE_FUNCTION
+    };
+
+    struct ConfigItem
+    {
+        ConfigType type;
+        const char* name;
+        union {
+                void* value_ptr;
+                void (*function)();
+            };
+        int increment;
+        int upper_limit;
+        int lower_limit;
+    };
+    
+    typedef void (*SettingInteracted)(ConfigItem*, int delta); // delta is the absolute increment of the config item, will be 0 if its a callback
+
     private:
     M5Canvas* _canvas;
     ExplorerTheme _theme;
-    SelectionCallback _callback = nullptr;
+    SettingInteracted _callback = nullptr;
     uint8_t _width = 0;
     uint8_t _half_width = 0;
     uint8_t _height = 0;
     int8_t _cursor_index = 0; // selection position
     uint16_t _cursor_offset = 0; // real position
     uint16_t _selection = 0; // global pos of selection
-    std::vector<String> _pathStack; // stores the current navigation path
-    std::vector<String> _dirList;  // stores what the current dir has
-    std::vector<bool> _isDirectory;   // parallel vector to remember if _dirList[i] is a folder
+    
+    ConfigItem* _pageStack[MAX_DEPTH];
+
     bool _had_theme = false; // set to true forever when default theme get overwritten
     bool _active = false; 
-    bool _has_dirs = false;
-    void _updateDirectoryList();
-    void _goToDir(String dir_name);
     void _goBack();
     void _render();
-    String _buildPath();
 
     public:
 
-    enum class Input : uint8_t{
-        up = 0,
-        down = 1,
-        left = 2,
-        right = 3,
-        back = 4,
-        select = 5
+    enum Input{
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT,
+        BACK,
+        SELECT
     };
 
-    void begin(M5Canvas* targetCanvas, SelectionCallback callback);
+    void begin(M5Canvas* targetCanvas, SettingInteracted callback);
     void setTheme(ExplorerTheme* theme = nullptr);
     void open(); // opens the file picker, wrapper or _active
     void close(); // closes the file picker
